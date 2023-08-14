719c8689b1f7ab27c81cb1f75cea5232583950cc
bass-bass <bounceback0505@gmail.com>
2023-08-14 13:20:01 +0000

    fix test

--- src/test/java/com/example/api/action/my/MyControllerTest.java
+++ src/test/java/com/example/api/action/my/MyControllerTest.java
@@ -4,14 +4,12 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-// 結合テスト
-//@SpringBootTest
-@PowerMockIgnore("com.example.api.common.logger.*")
+@SpringBootTest
 @AutoConfigureMockMvc
 public class MyControllerTest {
 	//@Test
